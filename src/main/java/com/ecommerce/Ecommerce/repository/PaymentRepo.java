31ec7e5f1c121139993762ccafdd7e4693a7e66a
SnomMobile <dennis21@my.yorku.ca>
2025-03-13 02:11:57 -0400

    Payment done
    Created the payment classes

---
+++ src/main/java/com/ecommerce/Ecommerce/repository/PaymentRepo.java
@@ -0,0 +1,7 @@
+package com.ecommerce.Ecommerce.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.ecommerce.Ecommerce.entity.Payment;
+
+public interface PaymentRepo extends JpaRepository<Payment, Long> {
+
+}
