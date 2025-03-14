6801e85aa783a88bc47d45bdc6538a57fafc150d
FRISBEE\death <deathscout2003@gmail.com>
2025-03-14 19:07:45 -0400

    fix category

--- src/main/java/com/ecommerce/Ecommerce/service/interf/CategoryService.java
+++ src/main/java/com/ecommerce/Ecommerce/service/interf/CategoryService.java
@@ -1,32 +1,10 @@
 package com.ecommerce.Ecommerce.service.interf;
 
 import com.ecommerce.Ecommerce.entity.Category;
-import com.ecommerce.Ecommerce.repository.CategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-@Service
-public class CategoryService {
-
-    private final CategoryRepository categoryRepository;
-
-    @Autowired
-    public CategoryService(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
-    }
-
-    public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
-    }
-
-    public Category createCategory(Category category) {
-        return categoryRepository.save(category);
-    }
-
-    public Category getCategoryById(Long id) throws Exception {
-        return categoryRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Category not found"));
-    }
+public interface CategoryService {
+    List<Category> getAllCategories();
+    Category createCategory(Category category);
+    Category getCategoryById(Long id);
 }
