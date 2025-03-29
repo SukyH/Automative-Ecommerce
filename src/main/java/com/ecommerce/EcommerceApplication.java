fad8cb14dcbc5bb1fd241b594d633ff631c201d1
SukyH <162163779+SukyH@users.noreply.github.com>
2025-03-29 04:55:40 -0400

    small fix

--- src/main/java/com/ecommerce/EcommerceApplication.java
+++ src/main/java/com/ecommerce/EcommerceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
